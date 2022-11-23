da2aa748c11345392ae4be678005d9a23ed7b154
Torsten Bonde Christiansen <tbch@universal-robots.com>
2022-11-23 21:58:53 +0100

    Initial commit for linux debians

---
+++ build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+project_dir=${PWD}
+
+version=$(xq . pom.xml | jq ".project.dependencies.dependency[0].version" | tr -d '"' )
+echo "Building Analysis: ${version}"
+./linux/build.sh "${project_dir}" "analysis" "${version}"
+
+version=$(xq . pom.xml | jq ".project.dependencies.dependency[1].version" | tr -d '"' )
+echo "Building EntryClient: ${version}"
+./linux/build.sh "${project_dir}" "entryclient" ${version}
+
+version=$(xq . pom.xml | jq ".project.dependencies.dependency[2].version" | tr -d '"' )
+echo "Building Manager: ${version}"
+./linux/build.sh "${project_dir}" "manager" ${version}
+
