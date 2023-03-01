8a4ea71e6607b048f1ed0659e85f0cf67dd357f2
Torsten Bonde Christiansen <tbch@universal-robots.com>
2023-03-01 22:54:51 +0100

    Added building mac versions

---
+++ darwin/build_zip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+project_dir="$1"
+component="$2"
+version="$3"
+
+component_dir="${project_dir}/${component}"
+basedir="$(dirname ${0})"
+build_dir="${project_dir}/target/build"
+output_dir="${project_dir}/target/output"
+
+epidata_component="epidata${component}"
+
+# copy binary
+mkdir -p "${build_dir}/"
+cp "${project_dir}/target/${component}/binaries/x86_64-darwin/"* "${build_dir}/"
+
+# copy documentation
+mkdir -p "${build_dir}/docs/"
+cp "${project_dir}/target/${component}/docs/"* "${build_dir}/docs/"
+cp "${project_dir}/target/docs/"* "${build_dir}/docs/"
+
+# copy samples
+mkdir -p "${build_dir}/samples/"
+cp "${project_dir}/target/samples/"* "${build_dir}/samples/"
+
+tar -zcv -C "${build_dir}" -f "${output_dir}/${epidata_component}.${version}.darwin.tgz" . > /dev/null 2>&1
+
+# clean up
+rm -fr "${build_dir}"
